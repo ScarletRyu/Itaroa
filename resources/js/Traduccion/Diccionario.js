4dca8ae1f5b2f581d1eb4cc41827ff2fd4074d85
jon <jpintool17dw@ikzubirimanteo.com>
2019-10-24 10:43:03 +0200

    traducciones modificadas

---
+++ resources/js/Traduccion/Diccionario.js
@@ -0,0 +1,17 @@
+var dict = {
+    "Restaurante":{
+        eng:"Restaurant",
+        eus:"Jatetxea",
+        esp:"Restaurante"			
+    },
+    "Tienda":{
+        eng:"Shop",
+        eus:"Denda",
+        esp:"Tienda"
+    },
+    "Ubicacion":{
+        eng:"Location",
+        eus:"Kokapena",
+        esp:"Ubicación"
+    }
+}
