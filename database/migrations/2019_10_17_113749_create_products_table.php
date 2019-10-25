25b2ff3cb99ebc0178c6481341a8f70ac227dfb5
cynda3 <andergonzalez33@gmail.com>
2019-10-25 11:08:36 +0200

    Seeders filled again

--- database/migrations/2019_10_17_113749_create_products_table.php
+++ database/migrations/2019_10_17_113749_create_products_table.php
@@ -16,7 +16,7 @@ class CreateProductsTable extends Migration
         Schema::create('products', function (Blueprint $table) {
             $table->bigIncrements('id');
             $table->string('name');
-            $table->string('description')->nullable();
+            $table->text('description')->nullable();
             $table->integer('stock');
             $table->float('price');
             $table->string('photo')->nullable();
---
+++ resources/views/elements/head.blade.php
@@ -0,0 +1,16 @@
+
+	<title>Itaro</title>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- Fonts -->
+    <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
+    <!-- Css -->
+	<link rel="stylesheet" type="text/css" href="css/estilo.css">
+	<link rel="stylesheet" type="text/css" href="css/tienda.css">
+    <!-- Scripts -->
+	<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+	<script src="js/jquery/jquery-3.1.1.js"></script>
+	<script src="js/efectos.js"></script>
+    <script type="text/javascript" src="js/Traduccion/jquery.translate.js"></script>
+    <script type="text/javascript" src="js/Traduccion/Diccionario.js"></script>
+    <script type="text/javascript" src="js/validation.js"></script>
