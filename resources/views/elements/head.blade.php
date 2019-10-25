39cc5e2bd73516b9d9089d7608938be7409b517a
cynda3 <andergonzalez33@gmail.com>
2019-10-25 12:21:50 +0200

    Intento fallido de layouts

--- resources/views/elements/head.blade.php
+++ resources/views/elements/head.blade.php
@@ -1,4 +1,8 @@
 
+
+<!-- no va -->
+
+
 	<title>Itaro</title>
 	<meta charset="utf-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
@@ -6,7 +10,9 @@
     <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
     <!-- Css -->
 	<link rel="stylesheet" type="text/css" href="css/estilo.css">
-	<link rel="stylesheet" type="text/css" href="css/tienda.css">
+	<link rel="stylesheet" type="text/css" href="css/tiendas.css">
+	<link rel="stylesheet" type="text/css" href="css/producto.css">
+	<link rel="stylesheet" type="text/css" href="css/formulario.css">
     <!-- Scripts -->
 	<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
 	<script src="js/jquery/jquery-3.1.1.js"></script>
