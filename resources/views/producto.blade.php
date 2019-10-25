4feb6c4fedcffc645e835411e3aac030439a01f2
nerea <nlabanderafe18dw@ikzubirimanteo.com>
2019-10-25 11:34:30 +0200

    estilos acabados

--- resources/views/producto.blade.php
+++ resources/views/producto.blade.php
@@ -5,9 +5,8 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
     <title>Itaroa</title>
-
-    <!-- Fonts -->
-    <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
+    <!-- Estilos -->
+    <link rel="stylesheet" type="text/css" href="/css/producto.css">
     <!-- Scripts -->
     <script src="/js/JQuery/jquery-3.1.1.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/jquery-validation@1.19.1/dist/jquery.validate.min.js"></script>
@@ -16,39 +15,34 @@
 
 </head>
 <body>
-    <div>
-        <!-- Imagen y nombre del producto -->
-        <h2>{{$product->name}} </h2>
-        <div id="producto">
-            <p>Producto: </p>
-            <p>{{$product->name}}</p>
-        </div>
-        <div id="description">
-            <p>Descripción: </p>
-            <p>{{$product->description}}</p>
-        </div>
-        <div id="precio">
-            <p>Precio: </p>
-            <p>{{$product->price}}€</p>
-        </div>
-        <div id="enlace">
-            <p>Enlace: </p>
-            <a href="{{$product->link}}"></a>
-        </div>
-        <div id="foto">
-            <p>Foto: </p>
-            <img src="{{$product->photo}}">
-        </div>
-    </div>
-    <div>
-        <!-- Formulario de modificación y visualización de datos -->
-        <form id="form" method="POST" action="{{ route('products.update',$product->id) }}" novalidate>
-            @csrf
-            @method('PUT')
+    <a href="{{URL::previous()}}"><img src="/img/arrow_back_icon.svg"></a>
+    <div id="contenido">
+        <!-- información del producto -->
+        <section id="info" >
+            <h2>{{$product->name}} </h2>
+            <article id="description">
+                <p>{{$product->description}}</p>
+            </article>
+            <article id="precio">
+                <p>{{$product->price}}€</p>
+            </article>
+            <article id="enlace">
+                <p>Visita la página oficial de la tienda aquí: </p>
+                <a href="{{$product->link}}"></a>
+            </article>
+            <form id="form" method="POST" action="{{ route('products.update',$product->id) }}" novalidate>
+                @csrf
+                @method('PUT')
 
-            <label>Stock: <label></label><input type="number" name="stock" value="{{$product->stock}}" ><br>
-            <button id="submit" type="submit" class="validar">Modificar</button>
-        </form>
+                <label>Stock: <label></label><input type="number" name="stock" value="{{$product->stock}}" >
+                <button id="submit" type="submit" class="validar">Modificar</button>
+            </form>
+        </section>
+        <section id="imagen">
+            <article>
+                <img src="{{$product->photo}}">
+            </article>
+        </section>   
     </div>
 </body>
 </html>
